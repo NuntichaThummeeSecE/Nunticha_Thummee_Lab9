d231e058cf93c3cc2d05f55343daa629c15b0a5e
NuntichaThummeeSecE <n01687478@humber.ca>
2024-10-31 17:13:12 -0400

    change api add for loop to get specific art objects

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
 //function geting data from the api//
 function getArtPictures() {
-    fetch(`https://collections.louvre.fr/ark:/53355/cl010277627.json`)
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11`)
         .then(response => response.json())
         .then(data => {
             //check data from the api//
             console.log(data);
-
-              // Get title of the image from the data
-              let imgTitle = data.title;
-              document.getElementById(`imageTitle`).textContent = imgTitle;
-  
-              // Get image from the data
-              let imgPicture = data.image[0].urlImage; // Make sure this index exists
-              document.getElementById(`art1`).src = imgPicture;
-  
-              // Get collection name
-              let collection = data.collection;
-              document.getElementById(`collection`).textContent = `Collection : ` + collection;
-  
-              // Get description from the data
-              let imgDescription = data.titleComplement || `-`;
-              document.getElementById(`imageDescription`).textContent = `Description : ` + imgDescription;
-  
-              // Get copyright info
-              let imgCopyright = data.image[0].copyright || `No copyright information available`;
-              document.getElementById(`imageCopyright`).textContent = imgCopyright;
-
         })
-        .catch(error => console.error(`Error fretching data: `, error));
+
+    //using for loop to access data id in api//
+    const artIds = data.objectIDs;
+    for (let i = 0; i < artIds.length; i++) {
+        const artId = artIds[i];
+
+        // Get specific art objects from th API//
+        fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + objectID)
+            .then(response => response.json())
+            .then(data => {
+
+                // Get title of the image from the data
+                let imgTitle = data.title;
+                document.getElementById(`imageTitle`).textContent = imgTitle;
+
+                // Get image from the data
+                let imgPicture = data.image[0].urlImage; // Make sure this index exists
+                document.getElementById(`art1`).src = imgPicture;
+
+                // Get collection name
+                let collection = data.collection;
+                document.getElementById(`collection`).textContent = `Collection : ` + collection;
+
+                // Get description from the data
+                let imgDescription = data.titleComplement || `-`;
+                document.getElementById(`imageDescription`).textContent = `Description : ` + imgDescription;
+
+                // Get copyright info
+                let imgCopyright = data.image[0].copyright || `No copyright information available`;
+                document.getElementById(`imageCopyright`).textContent = imgCopyright;
+
+            })
+            .catch(error => console.error(`Error fetching data: `, error));
+    }
 }
+
 getArtPictures();
 
