23ebf98e3ce662d40a25502e0c0d39710fbff8fa
NuntichaThummeeSecE <n01687478@humber.ca>
2024-10-31 15:50:36 -0400

    i got another information from api but it show in french

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 //function geting data from the api//
-function getArtPictures(){
+function getArtPictures() {
     fetch(`https://collections.louvre.fr/ark:/53355/cl010277627.json`)
-    .then(response => response.json())
-    .then (data => {
-        //check data from the api//
-        console.log(data);
-        
-        //get title of the img from the data//
-        let imgTitle = data.title;
-        document.getElementById(`imageTitle`).textContent = imgTitle;
+        .then(response => response.json())
+        .then(data => {
+            //check data from the api//
+            console.log(data);
 
-        //get img from the data//
-        let imgPicture = data.image[0].urlImage;
-        document.getElementById(`art1`).src = imgPicture;
-    })
-    .catch(error => console.error(`Error fretching data: `, error));
+            //get title of the img from the data//
+            let imgTitle = data.title;
+            document.getElementById(`imageTitle`).textContent = imgTitle;
+
+            //get img from the data//
+            let imgPicture = data.image[0].urlImage;
+            document.getElementById(`art1`).src = imgPicture;
+
+            let collection = data.collection;
+            document.getElementById(`collection`).textContent = collection;
+
+            //get description from the data//
+            let imgDescription = data.titleComplement || `No description available`;
+            document.getElementById(`imageDescription`).textContent = imgDescription;
+
+        })
+        .catch(error => console.error(`Error fretching data: `, error));
 }
 getArtPictures();
 
